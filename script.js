039dc0d13f838c6a49f7d348215da991e47c76a2
Beardo Mattix <christopher.mattix@gmail.com>
2021-07-29 10:35:26 -0400

    adding toggle to navbar burger

---
+++ script.js
@@ -0,0 +1,8 @@
+// // Mobile Menu
+
+const burgerIcon = document.querySelector("#burger");
+const navbarMenu = document.querySelector(".navbar-menu");
+
+burgerIcon.addEventListener("click", () => {
+    navbarMenu.classList.toggle('is-active')
+});
